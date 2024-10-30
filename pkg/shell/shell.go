fa74e46d2e847d2c302f30429a4af7651c3602f6
Will Daly <widaly@microsoft.com>
2024-10-30 14:40:09 -0700

    wip

--- pkg/shell/shell.go
+++ pkg/shell/shell.go
@@ -1,11 +1,13 @@
 package shell
 
 func RunInPod(podName, podNamespace string) error {
-	// TODO
+	// TODO: ephemeral pod in pod netns.
+	// TODO: how to get the image name/tag?
 	return nil
 }
 
 func RunInNode(nodeName string) error {
-	// TODO
+	// TODO: ephemeral pod in node.
+	// TODO: how to get the image name/tag?
 	return nil
 }
