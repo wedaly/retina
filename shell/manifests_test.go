cbd0e9dc741313cfee1ce69b9607a576eff2e023
Will Daly <widaly@microsoft.com>
2024-11-05 13:31:28 -0800

    wip

--- shell/manifests_test.go
+++ shell/manifests_test.go
@@ -18,9 +18,9 @@ func TestEphemeralContainerForPodDebug(t *testing.T) {
 	assert.Equal(t, []v1.Capability{"NET_ADMIN", "NET_RAW"}, ec.SecurityContext.Capabilities.Add)
 }
 
-func TestHostNetworkPodForNodeDebug(t *testing.T) {
+func TestHostNetworkPodForNodeDebugLinux(t *testing.T) {
 	config := Config{RetinaShellImage: testRetinaImage}
-	pod := hostNetworkPodForNodeDebug(config, "kube-system", "node0001")
+	pod := hostNetworkPodForNodeDebug(config, "kube-system", "node0001", "linux")
 	assert.True(t, strings.HasPrefix(pod.Name, "retina-shell-"), "Pod name does not start with the expected prefix")
 	assert.Equal(t, "kube-system", pod.Namespace)
 	assert.Equal(t, "node0001", pod.Spec.NodeName)
@@ -35,12 +35,12 @@ func TestHostNetworkPodForNodeDebug(t *testing.T) {
 	assert.Equal(t, 0, len(pod.Spec.Containers[0].VolumeMounts))
 }
 
-func TestHostNetworkPodForNodeDebugWithMountHostFilesystem(t *testing.T) {
+func TestHostNetworkPodForNodeDebugWithMountHostFilesystemLinux(t *testing.T) {
 	config := Config{
 		RetinaShellImage:    testRetinaImage,
 		MountHostFilesystem: true,
 	}
-	pod := hostNetworkPodForNodeDebug(config, "kube-system", "node0001")
+	pod := hostNetworkPodForNodeDebug(config, "kube-system", "node0001", "linux")
 	assert.True(t, strings.HasPrefix(pod.Name, "retina-shell-"), "Pod name does not start with the expected prefix")
 	assert.Equal(t, "kube-system", pod.Namespace)
 	assert.Equal(t, "node0001", pod.Spec.NodeName)
